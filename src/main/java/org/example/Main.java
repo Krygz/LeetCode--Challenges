fe6286b5c8ced4bb7b9b2ba4ea8bad2b6507f839
Pedro <ph4290245@gmail.com>
2024-10-10 17:20:56 -0300

    Lowest Common Ancestor of a Binary Search Tree

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,21 @@ import org.example.challenges.SearchBST;
 
 public class Main {
     public static void main(String[] args) {
-        SearchBST searchBST = new SearchBST();
-        SearchBST.TreeNode root = searchBST.new TreeNode(4);
-        root.left = searchBST.new TreeNode(2);
-        root.right = searchBST.new TreeNode(7);
-        root.left.left = searchBST.new TreeNode(1);
-        root.left.right = searchBST.new TreeNode(3);
-
-        int target = 2;
-        SearchBST.TreeNode result = searchBST.searchBST(root, target);
-
-        if (result != null) {
-            System.out.println("Valor encontrado: " + result.val);
-        } else {
-            System.out.println("Valor " + target + " não encontrado na árvore.");
-        }
+            //SEARCHBST
+//        SearchBST searchBST = new SearchBST();
+//        SearchBST.TreeNode root = searchBST.new TreeNode(4);
+//        root.left = searchBST.new TreeNode(2);
+//        root.right = searchBST.new TreeNode(7);
+//        root.left.left = searchBST.new TreeNode(1);
+//        root.left.right = searchBST.new TreeNode(3);
+//
+//        int target = 2;
+//        SearchBST.TreeNode result = searchBST.searchBST(root, target);
+//
+//        if (result != null) {
+//            System.out.println("Valor encontrado: " + result.val);
+//        } else {
+//            System.out.println("Valor " + target + " não encontrado na árvore.");
+//        }
     }
 }
---
+++ src/main/java/org/example/challenges/LCA.java
@@ -0,0 +1,39 @@
+package org.example.challenges;
+public class LCA {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    /*
+    root = raiz
+    p = node da esquerda;
+    q = node da direita;
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        /*se root.val é maior que "p" e maior que "q",
+        isso significa que ambos estão no node
+        da esquerda
+        * */
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        /*se root.val é menor que "p" e menor que "q",
+        isso significa que ambos estão no node
+        da direita
+        * */
+        else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        /*
+        Se nenhuma das condições anteriores for atendida,
+        então encontramos o nosso menor ancestral comum.
+        */
+        else {
+            return root;
+        }
+    }
+}
+
+
